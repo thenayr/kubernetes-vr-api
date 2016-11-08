4427c226c2c79c4c32f39cf7a4aae3363d3e01cc
Ryan vanniekerk <iamnayr@gmail.com>
2016-11-08 13:24:30 -0800

    Newly added pods send type correctly

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ const jsonStream = new JSONStream();
 const pods = fs.readFileSync("pods.json");
 const podJson = JSON.parse(pods);
 const sleep = require('sleep');
+var clusterInfo = {
+  podCount: null,
+  deployCount: null
+}
 
 const server = app.listen(9003, () => {
     console.log('Listening on *:9003');
@@ -20,21 +24,49 @@ const K8Api = require('kubernetes-client');
 const options = {
   url: 'http://127.0.0.1:8090',
   version: 'v1',  // Defaults to 'v1' 
-  namespace: 'default' // Defaults to 'default' 
+  namespace: 'default', // Defaults to 'default' 
 }
 
+// Ugly but works for now (the stream was timing out before)
+const streamOptions = {
+  url: options.url,
+  version: options.version,  // Defaults to 'v1' 
+  namespace: options.namespace, // Defaults to 'default' 
+  request: {
+    timeout: 0,
+    forever: true,
+  }
+}
+
+const deployOptions = {
+  url: options.url,
+  version: 'v1beta1',  // Defaults to 'v1' 
+  namespace: options.namespace, // Defaults to 'default' 
+}
+
+const k8Stream = new K8Api.Core(streamOptions);
 const k8 = new K8Api.Core(options);
+const k8deploy = new K8Api.Extensions(deployOptions);
 
 function getKubeStream() {
-
-  const stream = k8.ns.po.get({ qs: { watch: true } });
+  const stream = k8Stream.ns.po.get({ qs: { watch: true } });
   stream.pipe(jsonStream);
   jsonStream.on('data', object => {
 
     switch(object.type) {
       case 'ADDED':
+        var type = '';
+        if ("object.metadata.labels.type" in object) {
+          type = object.object.metadata.labels.type
+        } else {
+          type = "k8s"
+        }
         var pod = {
-          name: object.object.metadata.name 
+          name: object.object.metadata.name ,
+          ip: object.object.status.podIP,
+          start: object.object.status.startTime,
+          namespace: object.object.metadata.namespace,
+          type: type
         }
         console.log(pod.name + " Added");
         io.emit('newPod' , pod);
@@ -47,19 +79,19 @@ function getKubeStream() {
         io.emit('removePod' , pod);
         break;
       default:
-      console.log("Differnet state - " + object.object.kind + " was " + object.type);
+      // console.log("Differnet state - " + object.object.kind + " was " + object.type);
     }
   });
 }
 
 function fetchPods(err, result) {
   if (err){
-    console.log("Cannot connect to Kubernetes")
+    console.log("Cannot connect to Kubernetes for pods")
     err;
   } else {
     const items = result.items;
     var pods = {};
-    podList = Array.from(items, i => ({name: i.metadata.name}) );
+    podList = Array.from(items, i => ({ name: i.metadata.name, ip: i.status.podIP, start: i.status.startTime, namespace: i.metadata.namespace, type: "nginx"}) );
     podsResponse = { podList };
     console.log(podsResponse);
     // Sometimes this event fires too quickly, lets sleep for a half second
@@ -68,6 +100,38 @@ function fetchPods(err, result) {
     console.log("sent pod init");
   }
 }
+
+function fetchClusterInfo() {
+  // pods = k8.ns.po.get(fetchPods);
+  k8deploy.ns.deployments.get(function (err, deployments) {
+    if (err){
+      console.log("Cannot connect to Kubernetes for deploys")
+      console.log(err);
+    } else {
+      const items = deployments.items;
+      deployList = Array.from(items, i => ({name: i.metadata.name}) );
+      deploymentResponse = { deployList };
+      deployCount = deployList.length;
+      k8.ns.po.get(function (err, pods) {
+        if (err){
+          console.log("Cannot connect to Kubernetes for pods")
+          err;
+        } else {
+        const items = pods.items;
+        podList = Array.from(items, i => ({name: i.metadata.name})) ;
+        podResponse = { podList };
+        podCount = podList.length;
+        var clusterInfo = {
+          pods: { count: podCount},
+          deployments: { count: deployCount }
+        }
+        console.log("Emitting cluster data")
+        io.emit('clusterData', clusterInfo )
+        }
+      })
+    }
+  }
+)};
 
 
 
@@ -79,6 +143,10 @@ io.on('connection', function(socket) {
     socket.on('sniffPods', function() {
       console.log("Got sniff pods command");
       k8.ns.po.get(fetchPods);
+    });
+    socket.on('fetchClusterInfo', function() {
+      console.log("Got cluster info fetch command");
+      fetchClusterInfo();
     });
     socket.on('k8sDestroyPod', function(data) {
       console.log(data); 
